1479b185c3895299415eabbcdcd886c5e31853ff
Andile Jaden Mbele <andilembele020@gmail.com>
2021-10-11 20:58:07 +0200

    ForNextiterations

---
+++ ForNextIteration/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        For index = 1 To 10
+            Console.WriteLine(index)
+        Next
+
+        Console.ReadLine()
+    End Sub
+End Module
