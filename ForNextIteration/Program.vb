210da16c29c5bd70ff9fcd48bf8fd2792f95ee14
Andile Jaden Mbele <andilembele020@gmail.com>
2021-10-12 12:35:59 +0200

    ForNextIteration

--- ForNextIteration/Program.vb
+++ ForNextIteration/Program.vb
@@ -3,6 +3,10 @@ Imports System
 Module Program
     Sub Main(args As String())
         For index = 1 To 10
+            If index = 7 Then
+                Console.WriteLine("Found 7")
+                Exit For
+            End If
             Console.WriteLine(index)
         Next
 
