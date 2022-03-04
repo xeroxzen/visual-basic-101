31011770a36afdf54c593f9e95aa2ba603cf1b83
Google Jr <andilembele020@gmail.com>
2022-03-04 04:07:05 +0200

    Calculate Monthly Salary

---
+++ Calculate Monthly Salary/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim hoursWorked As Integer
+        Dim hourlyWage As Double
+        Dim salary As Double
+        Dim daysWorked As Integer = 28
+
+        hoursWorked = TextBox1.Text
+        hourlyWage = TextBox2.Text
+        If hourlyWage And hourlyWage <> vbInteger Or vbDouble Then
+            salary = (hoursWorked * hourlyWage) * daysWorked
+        Else
+            MsgBox("Hourly wage or hours is not a valid number.")
+        End If
+
+
+        TextBox3.Text = "$" & salary
+
+    End Sub
+End Class
